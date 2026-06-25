9547ade5147731ac9f3986887dfb3bd97b895607
vladlevin790 <vladlevin790@gmail.com>
2026-06-25 19:59:45 +0300

    fix test-issue

--- config/config.exs
+++ config/config.exs
@@ -2,10 +2,12 @@ import Config
 
 config :beamcore, :rate_limit_ms, 1000
 config :beamcore, :provider_receive_timeout_ms, 300_000
+config :beamcore, :mesh_enabled, true
 
 if Config.config_env() == :test do
   config :beamcore, :completions_module, Beamcore.Agent.MockCompletions
   config :beamcore, :rate_limit_ms, 0
   config :beamcore, :memory_dets_path, "tmp/test_memory.dets"
   config :beamcore, :config_dets_path, "tmp/test_config.dets"
+  config :beamcore, :mesh_enabled, false
 end
