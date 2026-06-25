45750365a6f16ad9474e2b690cc182d3a5f16aed
vladlevin790 <vladlevin790@gmail.com>
2026-06-26 00:57:32 +0300

    update key evenets

--- lib/tui/events/key_events.ex
+++ lib/tui/events/key_events.ex
@@ -1,7 +1,7 @@
 defmodule Beamcore.TUI.Events.KeyEvents do
   @moduledoc false
 
-  @ignored_kinds ["release", :release, "repeat", :repeat]
+  @ignored_kinds ["release", :release]
 
   def actionable?(%ExRatatui.Event.Key{kind: kind}), do: kind not in @ignored_kinds
   def actionable?(%{kind: kind}), do: kind not in @ignored_kinds
